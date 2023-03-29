04c7069163be1f4687637d9ed267f84b14a51b1a
Bruno Pires <bruno.pires.1096@gmail.com>
2023-03-29 20:56:23 -0300

    Setting styled components

---
+++ frontend/src/components/Home/index.jsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+      <Styled.Wrapper background="blue">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
+  );
+}
+
+export default Home;
--- frontend/src/components/index.jsx
+++
@@ -1,8 +0,0 @@
-function Home () {
-    return (
-        <h1>teste</h1>
-    )
-}
-
-
-export default Home;
