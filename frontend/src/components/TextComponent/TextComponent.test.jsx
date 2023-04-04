bf992640353af4d94a06e47045e12a6b5723e0c4
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-04 19:11:56 -0300

    Create TextComponent and your Test

---
+++ frontend/src/components/TextComponent/TextComponent.test.jsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { TextComponent } from '.';
+import { expect, it } from 'vitest';
+
+describe('<TextComponent />', () => {
+  it('render a text', () => {
+    renderTheme(<TextComponent>Children</TextComponent>);
+    expect(screen.getByText('Children')).toBeInTheDocument();
+  });
+
+  it('match snapshot', async () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+
+    // expect(container.firstChild).toMatchSnapshot(); //gena a pasta e arquivo de snapshot
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 1.5rem;
+      }
+
+      <p
+        class="c0"
+      >
+        Children
+      </p>
+    `); //gera o snapshot aqui dentro
+  });
+});
